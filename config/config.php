23bed64538589789ffa7c638bd7f7c544f635e64
Hugi <nguyengiahung1403@gmail.com>
2025-10-04 14:03:01 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php 
-    define("IS_DEBUG", false);
+    define("IS_DEBUG", true);
     define("IS_LOCAL", false);
     define('ROOT_PATH', dirname(__DIR__));
     define('LOG_PATH', ROOT_PATH . '/logs');
